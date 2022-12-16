e4cf6e23edba9647df3c2d80f3c52c253a10f998
Jay Chen <110469261+PogFishGaming@users.noreply.github.com>
2022-12-16 10:36:36 -0500

    aesfdasfafasefasfasedafesdaf

--- app/app/Scripts/activate.bat
+++ app/app/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\eric.wang\Documents\GitHub\api-projejct-eric-and-jay\app\app
+set VIRTUAL_ENV=C:\Users\jay.chen\Documents\GitHub\api-projejct-eric-and-jay\app\app
 
 if not defined PROMPT set PROMPT=$P$G
 

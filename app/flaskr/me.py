4d985b8bda2c3316dc9558b66031df0b6ec689bc
Jay Chen <110469261+PogFishGaming@users.noreply.github.com>
2023-01-04 10:26:58 -0500

    dawsdaw

---
+++ app/flaskr/me.py
@@ -0,0 +1,21 @@
+me = {"header": "Pictures of dogs",
+    "sub_header": "",
+    "interest1": {
+        "title": "",
+        "image":"../static/images/",
+        "description": ""
+    },
+    "interest2": {
+    "title": "",
+    "image": "../static/images/",
+    "description": ""
+    },
+    "interest3": {
+    "title": "",
+    "image": "../static/images/",
+    "description": ""
+    },
+    "meme": {
+    "image":"../static/images/"
+    }
+    }
